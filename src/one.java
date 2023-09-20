d24c9e33b9fcc221076f181afb8381938f96624e
maulik savaliya <maulikkumar955@gmail.com>
2023-09-20 16:11:26 +0100

    2 commit

--- src/one.java
+++ src/one.java
@@ -1,11 +1,18 @@
 public class one {
     public static void main(String[] args) {
         cocatinationstring("maulik","savaliya");
+        samesequance("maulik savaliya","savaliya");
     }
     //concatination
     static void cocatinationstring(String name, String surname){
         System.out.println("string 1 :" + name);
         System.out.println("string 2 :" + surname);
         System.out.println("concatinate string :" + name + surname);
+    }
+    // to find same sequance
+    static void samesequance(String name,String surname){
+        System.out.println("String 1 :" + name);
+        System.out.println("String 2 :" + surname);
+        System.out.println("to find same sequance :" + name.contains(surname));
     }
 }
