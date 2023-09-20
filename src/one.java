3b526284949b6a9946dd57decf4ea83ad5b9b531
maulik savaliya <maulikkumar955@gmail.com>
2023-09-20 15:56:03 +0100

    1 commit

---
+++ src/one.java
@@ -0,0 +1,11 @@
+public class one {
+    public static void main(String[] args) {
+        cocatinationstring("maulik","savaliya");
+    }
+    //concatination
+    static void cocatinationstring(String name, String surname){
+        System.out.println("string 1 :" + name);
+        System.out.println("string 2 :" + surname);
+        System.out.println("concatinate string :" + name + surname);
+    }
+}
